2c83d96f77b46ce19faa6183b8b2471b459c776d
Dustin Steinhauer <dustin.steinhauer@1010data.com>
2024-01-08 22:36:02 -0500

    get_players route

--- server/api/join.py
+++ server/api/join.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from models import players, max_players, Player
 
 join = Blueprint('join', __name__)
@@ -14,3 +14,16 @@ def join_game():
     new_player = Player(id, data['name'])
     players.append(new_player)
     return {"player_id": id}
+
+
+@join.route('/api/get_players', methods=['POST'])
+def get_players():
+    data = request.get_json()
+
+    player_view = []
+    for p in range(max_players):
+        try:
+            player_view.append(players[(p + data['player_id']) % max_players])
+        except IndexError:
+            player_view.append(Player(-1, 'null'))
+    return jsonify([player.to_dict() for player in player_view])
--- server/models.py
+++ server/models.py
@@ -5,3 +5,6 @@ class Player:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name}
