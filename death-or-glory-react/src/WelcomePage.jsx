98af570f331ac6ed53317ee1f7bd5b077a1a393c
DJ <davidjpace@gmail.com>
2024-06-03 22:25:18 -0400

    New React Trial

---
+++ death-or-glory-react/src/WelcomePage.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './WelcomePage.css';
+
+function WelcomePage() {
+    const [playerName, setPlayerName] = useState('');
+    const [welcomeMessage, setWelcomeMessage] = useState('');
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const navigate = useNavigate();
+
+    const handleInputChange = (event) => {
+        setPlayerName(event.target.value);
+    };
+
+    const handleButtonClick = () => {
+        setWelcomeMessage(`Welcome, ${playerName}!`);
+        setIsSubmitted(true);
+    };
+
+    const handleNavigate = () => {
+        navigate('/game'); // Navigate to the new page
+    };
+
+return (
+    <div className="WelcomePage">
+        <h1>Death or Glory</h1>
+        {!isSubmitted ? (
+            <>
+        <div>
+            Enter Player Name
+        </div>
+        <input type="text" onChange={handleInputChange} />
+        <button onClick={handleButtonClick}>
+                Submit
+            </button>
+            </>
+        ) : (
+            <>
+            <div className="welcomemessage">
+                {welcomeMessage}
+             </div>
+                <div>
+                <button className="JoinGame" onClick={handleNavigate}>Join Game</button>
+                </div>
+                </>
+        )}
+    </div>
+);
+}
+
+export default WelcomePage
