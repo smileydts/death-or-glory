cf35d407f79d86ce00a8b4c7a78a835a8c49d63c
djpace14 <154949313+djpace14@users.noreply.github.com>
2024-09-06 21:42:24 -0400

    Gamewindowelements (#35)
    * New React Trial
    
    * Dockerize new React-based webpage
    
    * volume for node_modules
    
    * new components added
    
    * Changes to layout of game window
    
    * New Card and Button Features
    
    * Added Player Stats
    
    * Dice Roll Added
    
    ---------
    
    Co-authored-by: Dustin Steinhauer <dustin.steinhauer@1010data.com>

--- client/src/TurnTracker.jsx
+++ client/src/TurnTracker.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './TurnTracker.css'; // Import CSS file for styling
 
 const TurnTracker = () => {
+  const boxes = [];
+  for (let i = 1; i <= 9; i++) {
+    boxes.push(<div key={i} className="box">{i}</div>);
+  }
+
   return (
-    <div>
-       <div className="box1">1</div>
-        <div className="box2">2</div>
-        </div>
+    <div className="turncontainer">
+      <div className="turn-label">Turn</div>
+      {boxes}
+    </div>
   );
 };
 
